b64cf686ff26aed96ae607c44e3a145effe35e26
Bruno Alves <vespidhook@gmail.com>
2020-03-31 14:10:12 -0300

    up

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# login-facebook
+# Login com o Facebook
 Exemplo de login com facebook com OAuth em PHP
---
+++ source/Config.php
@@ -0,0 +1,10 @@
+<?php
+
+session_start();
+
+define("FACEBOOK", [
+  "app_id" => "605343146989393",
+  "app_secret" => "e6b8c7434a5cd4808b59ff77249c28ae",
+  "app_redirect" => "https://www.milcontas.com.br/testelogin/",
+  "app_version" => "v4.0"
+]);
