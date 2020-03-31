cde3ae25882ad9cce7073e89cd098822aaf4e50e
Bruno Alves <vespidhook@gmail.com>
2020-03-31 14:11:10 -0300

    gitando

--- source/Config.php
+++ source/Config.php
@@ -3,8 +3,8 @@
 session_start();
 
 define("FACEBOOK", [
-  "app_id" => "605343146989393",
-  "app_secret" => "e6b8c7434a5cd4808b59ff77249c28ae",
+  "app_id" => "60534314698939",
+  "app_secret" => "e6b8c7434a5cd4808b59ff77249c28a",
   "app_redirect" => "https://www.milcontas.com.br/testelogin/",
   "app_version" => "v4.0"
 ]);
